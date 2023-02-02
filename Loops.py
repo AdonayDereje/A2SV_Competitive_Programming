98abfa76ce21ff53adecd63255fe079943d7222b
AdonayDereje <81191245+AdonayDereje@users.noreply.github.com>
2023-02-02 11:03:33 +0300

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -3,8 +3,7 @@ if __name__ == '__main__':
 
     if n < 1 or n > 20:
         print ("out of range") 
-#
-
+#there is a constraint for the input
     else:
         for values in range(n):
             print(values * values)
